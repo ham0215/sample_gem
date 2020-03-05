0a6f8e0e782530841cd735c6a43b3ce9b625eccd
naoto hamada <hamada@vis-its.com>
2020-03-05 16:11:51 +0900

    Add class method, instance method

--- lib/sample_gem.rb
+++ lib/sample_gem.rb
@@ -1,6 +1,29 @@
 require "sample_gem/version"
 
 module SampleGem
-  class Error < StandardError; end
-  # Your code goes here...
+  SAMPLE_CONST='これはサンプルです'
+
+  class << self
+    def increment(num)
+      raise NoIntegerError unless num == num.to_i
+
+      num + 1
+    end
+
+    def sample_const
+      SAMPLE_CONST
+    end
+  end
+
+  def initialize(num)
+    @num = num
+  end
+
+  def increment
+    raise NoIntegerError unless num == num.to_i
+
+    @num += 1
+  end
+
+  class NoIntegerError < StandardError; end
 end
